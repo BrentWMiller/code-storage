435cfe7a20217d090b677771ada4f4a295fcaa77
BrentWMiller <brentwmiller93@gmail.com>
2023-01-10 22:41:05 -0500

    💄 Removes fira code everywhere

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"FiraCode"', 'monospace'],
+        heading: ['"FiraCode"', 'monospace'],
       },
       colors: {
         theme: {
