2773ff5521a23f1f28cb778c2890e5a325dcea0c
BrentWMiller <brentwmiller93@gmail.com>
2023-01-12 22:50:11 -0500

    💄 Makes layout not require heading or description

--- src/components/global/Layout.tsx
+++ src/components/global/Layout.tsx
@@ -9,11 +9,13 @@ type Props = {
 
 const Layout = ({ heading, description, container = false, children }: Props) => {
   return (
-    <section className='w-full h-full'>
-      <div className='p-8 pt-16 flex flex-col gap-1.5'>
-        {heading && <h1 className='text-3xl font-bold font-heading'>{heading}</h1>}
-        {description && <p className='text-base text-white/80'>{description}</p>}
-      </div>
+    <section className='relative w-full h-full pt-16'>
+      {(heading || description) && (
+        <div className='p-8 pt-0 flex flex-col gap-1.5'>
+          {heading && <h1 className='text-3xl font-bold font-heading'>{heading}</h1>}
+          {description && <p className='text-base text-white/80'>{description}</p>}
+        </div>
+      )}
 
       {container ? <div className='px-8'>{children}</div> : children}
     </section>
