81b1fb1ab9853edf955277ef87acb6eabe49a0d1
BrentWMiller <brentwmiller93@gmail.com>
2023-01-22 21:58:17 -0500

    🔧 adds default file ignoring

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,6 +3,7 @@ export const appConfig = {
   DEFAULT_LANGUAGE_ID: 'javascript',
   DEFAULT_SNIPPETS_FOLDER: 'snippets',
   DEFAULT_README_FILENAME: 'README.md',
+  DEFAULT_FILE_IGNORE_LIST: ['.DS_Store'],
   LANGUAGE_ICONS_FOLDER: '/language-icons',
   LANGUAGES: [
     { value: 'javascript', label: 'JavaScript' },
